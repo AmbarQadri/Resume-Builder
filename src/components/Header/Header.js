a4287297e198c73d6333aa61ff0beca19c4d85ce
Mohammed Ambar Qadri <144211207+AmbarQadri@users.noreply.github.com>
2024-06-04 20:11:32 +0530

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import resumeSvg from "../../assets/resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume Builder</span> for AIKTC Students!
+        </p>
+        <br></br>
+        <p className={styles.heading}>
+          Make your career <span>Bright</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
